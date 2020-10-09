442758919e06db756383b15d0b270311b1ce4746
김다솜 <95kds@naver.com>
2020-10-10 00:14:10 +0900

    eslint 설정 , state ver1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+// import PropTypes from 'prop-types';
 
-class App extends React.Component{
-  state = {
-    count : 0 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("hello");
   }
+    state = {
+      count: 0,
+    };
+ 
   add = () => {
-    this.state.count = 1;
+    this.setState(current => ({count: current.count +1}));
   };
+
   minus = () => {
-    this.state.count = -1;
+    this.setState(current => ({count: current.count -1}));
   };
-  render(){
+componentDidMount(){
+  console.log("component rendered");
+}
+componentDidUpdate(){
+  console.log("Did update")
+}
+componentWillUnmount(){
+  console.log("Goodbye, cruel world!")
+}
+  render() {
+    console.log("I'm render");
     return (
       <div>
-        <h1>The number is: {this.state.count}</h1>
+        <h1>
+          The number is:
+          {this.state.count}
+        </h1>
         <button onClick={this.add}>Add</button>
         <button onClick={this.minus}>Minus</button>
       </div>
-    )
+    );
   }
 }
 

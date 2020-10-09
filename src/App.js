6550f8fdff246a91ac02fa964174329835ff0f2a
김다솜 <95kds@naver.com>
2020-10-10 08:57:33 +0900

    영화 정보 불러오기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
-    isLoading :true
+    isLoading :true,
+    movies: []
   };
+  getMovies = async () =>{
+    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies);
+    //1. axios로 불러와진 데이터는 처음에 어디에 담기지?
+    //2. 콘솔에 movies를 어떻게 바로찍을 수 있는지?
+
+    this.setState({movies, isLoading:false})
+    // movies = movies : movies 
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000)
+    this.getMovies();
   }
   render() {
     return (
